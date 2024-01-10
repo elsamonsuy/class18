27c8fde7b38b71382427b1e6038c1f03d1567535
Elsa Monsuy <elsamonsuy@yahoo.com>
2024-01-10 06:18:22 +0000

    change was make diff

--- simple.sh
+++ simple.sh
@@ -3,5 +3,5 @@
 echo "hello world!"
 echo "1 2 3 4 5 6 7 8 0"
 sleep 20
-## print " hello world" to my screen
+## print " hello world" to my screen.this is supposed to display hello world on the screen
 ##### we are understanding git branching and margeing:
