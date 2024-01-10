74f75776f43e1b81f82d234ad24fba81699efdce
Elsa Monsuy <elsamonsuy@yahoo.com>
2024-01-10 06:13:14 +0000

    learning merge

--- simple.sh
+++ simple.sh
@@ -3,5 +3,5 @@
 echo "hello world!"
 echo "1 2 3 4 5 6 7 8 0"
 sleep 20
-## print " hello world" to my screen
+## print " hello world" to my screen and diplay the output
 ##### we are understanding git branching and margeing:
